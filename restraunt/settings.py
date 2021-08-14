44484ed408fa2d21d3d9843c1441b078f799e23f
kosi2109 <nolockfacebook55@gmail.com>
2021-08-14 23:03:39 +0630

    final

--- restraunt/settings.py
+++ restraunt/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9)_$t1xrpi9p73))!tntwop&#pu23-wzh6m*n^rfb65h7+$24!'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['nyein.herokuapp.com']
 
